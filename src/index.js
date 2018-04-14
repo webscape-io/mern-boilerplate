9f28e0f5e4b2f8a8aa69fc9a8cfa85148748bb70
Conner Aiken <conner@fittedtech.com>
2018-04-13 22:09:39 -0700

    Add request and scraping library"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import express from 'express';
 import utils from "./helpers/utils.js";
 import path from "path";
 import dotenv from "dotenv";
@@ -7,14 +6,4 @@ global.path = path;
 global.dotenv = dotenv;
 
 utils.loadENV();
-const app = express();
 
-app.use(express.static(path.resolve(process.cwd(), 'public')))
-
-app.get('/api', (req, res) => {
-    res.send('Express to the rescue!');
-});
-
-app.listen(process.env.port, () => {
-    utils.log(`Server has started and is listening on port ${process.env.port}!`)
-});
